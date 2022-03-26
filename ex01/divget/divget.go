757e50a02e036eae25231b3f2da0ee39d03cf284
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-03-27 02:22:53 +0900

    fix: use pb package

--- ex01/divget/divget.go
+++ ex01/divget/divget.go
@@ -2,9 +2,11 @@ package divget
 
 import (
 	"context"
-	"fmt"
+	"os"
 
+	"github.com/cheggaaa/pb/v3"
 	"golang.org/x/sync/errgroup"
+	"golang.org/x/term"
 )
 
 var eg errgroup.Group
@@ -19,7 +21,22 @@ func Run(ctx context.Context, url string, divN uint64) error {
 	// 前準備
 	data := loadCache(cf)
 
-	fmt.Println(data)
+	// progress
+	// makeProgressbar
+	width, _, err := term.GetSize(0)
+	width /= 4
+	if err != nil {
+		return err
+	}
+	// var bars []*pb.ProgressBar
+	// for _, d := range data {
+	// 	// pbi := pb.New(int(d.to - d.from)).SetMaxWidth(width)
+	// 	pbi := pb.Start64(int64(d.to-d.from)).SetMaxWidth(width).SetWriter(os.Stderr).Set(pb.Bytes, true)
+	// 	bars = append(bars, pbi)
+	// }
+	// fmt.Println(len(bars))
+	// os.Exit(1)
+	bar := pb.Start64(int64(cf.fileSize)).SetMaxWidth(width).SetWriter(os.Stderr).Set(pb.Bytes, true)
 
 	eg, ctx := errgroup.WithContext(ctx)
 	for i := uint64(0); i < cf.divN; i++ {
@@ -29,7 +46,7 @@ func Run(ctx context.Context, url string, divN uint64) error {
 			case <-ctx.Done():
 				return ctx.Err()
 			default:
-				if err := divDownload(&data[i], cf.url, cf.filePath, i); err != nil {
+				if err := divDownload(bar, &data[i], cf.url, cf.filePath, i); err != nil {
 					return err
 				}
 				return nil
--- ex01/divget/download.go
+++ ex01/divget/download.go
@@ -4,33 +4,32 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"net/http/httputil"
 	"os"
 
-	"github.com/schollz/progressbar/v3"
+	"github.com/cheggaaa/pb/v3"
 )
 
-func save(path string, dataRange *byteRange, resp *http.Response) error {
+func save(bar *pb.ProgressBar, path string, dataRange *byteRange, resp *http.Response) error {
 	f, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	defer f.Close()
 	if err != nil {
 		return err
 	}
+	defer f.Close()
+
 	_, err = f.Seek(0, 2)
 	if err != nil {
 		return err
 	}
 
-	bar := progressbar.DefaultBytes(
-		resp.ContentLength,
-		"downloading",
-	)
+	// display progress bar
+	barReader := bar.NewProxyReader(resp.Body)
+	io.Copy(f, barReader)
+	bar.Finish()
 
-	io.Copy(io.MultiWriter(f, bar), resp.Body)
 	return nil
 }
 
-func divDownload(dataRange *byteRange, url, filePath string, index uint64) error {
+func divDownload(bar *pb.ProgressBar, dataRange *byteRange, url, filePath string, index uint64) error {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return err
@@ -38,8 +37,8 @@ func divDownload(dataRange *byteRange, url, filePath string, index uint64) error
 
 	req.Header.Set("Range", fmt.Sprintf("bytes=%d-%d", dataRange.from, dataRange.to))
 
-	dump, _ := httputil.DumpRequestOut(req, true)
-	fmt.Printf("%s\n", dump)
+	// dump, _ := httputil.DumpRequestOut(req, true)
+	// fmt.Printf("%s\n", dump)
 
 	client := new(http.Client)
 	resp, err := client.Do(req)
@@ -48,5 +47,5 @@ func divDownload(dataRange *byteRange, url, filePath string, index uint64) error
 	}
 
 	defer resp.Body.Close()
-	return save(fmt.Sprintf("./.cache/%s_%d", filePath, index), dataRange, resp)
+	return save(bar, fmt.Sprintf("./.cache/%s_%d", filePath, index), dataRange, resp)
 }
